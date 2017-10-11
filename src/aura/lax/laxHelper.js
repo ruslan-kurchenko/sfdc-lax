e3d9e3772eeab3e1eb740dbfd1acc0bcca2ab093
Ruslan Kurchenko <ruslan.kurchenko.contractor@weather.com>
2017-10-11 11:21:23 +0300

    Add ESLint support

--- src/aura/lax/laxHelper.js
+++ src/aura/lax/laxHelper.js
@@ -1,72 +1,69 @@
 ({
-    init: function (component, event) {
-        // ready event
-        console.log('Lax "init" function');
+  init: function init(component) {
+    // ready event
+    console.log('Lax "init" function');
 
 
-        // context component
-        const context = component.get('v.context');
-        const lax = this.getLax();
+    // context component
+    const context = component.get('v.context');
+    const lax = this.getLax();
 
-        // dynamic getter
-        const laxGetter = function () {
-            lax.setContext(this);
-            return lax;
-        };
-        Object.defineProperty(context, "lax", { get: laxGetter });
-    },
+    // dynamic getter
+    const laxGetter = function laxGetter() {
+      lax.setContext(this);
+      return lax;
+    };
+    Object.defineProperty(context, 'lax', { get: laxGetter });
+  },
 
-    getLax: function () {
-        // return lax if it is already instantiated
-        if (this.lax) return this.lax;
+  getLax: function getLax() {
+    // return lax if it is already instantiated
+    if (this.lax) return this.lax;
 
-        this.lax = (function(){
+    this.lax = (function Lax() {
+      function setContext(component) {
+        this.context = component;
+      }
 
-            function setContext(component) {
-                this.context = component;
+      function enqueue(actionName, params, options) {
+        return new Promise($A.getCallback((resolve, reject) => {
+          const action = this.context.get(actionName);
+
+          if (params) {
+            action.setParams(params);
+          }
+
+          if (options) {
+            if (options.isBackground) action.setBackground();
+            if (options.isStorable) action.setStorable();
+          }
+
+          action.setCallback(this.context, (response) => {
+            const state = response.getState();
+
+            if (this.context.isValid() && state === 'SUCCESS') {
+              resolve(response.getReturnValue());
+            } else {
+              reject(response.getError());
             }
+          });
+          $A.enqueueAction(action);
+        }));
+      }
 
-            function enqueue(actionName, params, options) {
-                return new Promise($A.getCallback((resolve, reject) => {
-                    const action = this.context.get(actionName);
+      function enqueueAll(actions) {
+        const promises = actions.map(a => enqueue.call(this, a.name, a.params, a.options));
 
-                    if (params) {
-                        action.setParams(params);
-                    }
+        return Promise.all(promises);
+      }
 
-                    if (options) {
-                        if (options.isBackground) action.setBackground();
-                        if (options.isStorable) action.setStorable();
-                    }
+      return {
+        setContext: setContext,
+        enqueue: enqueue,
+        enqueueAll: enqueueAll,
+      };
+    }());
 
-                    action.setCallback(this.context, (response) => {
-                        const state = response.getState();
-
-                        if (this.context.isValid() && state === 'SUCCESS') {
-                            resolve(response.getReturnValue());
-                        } else  {
-                            reject(response.getError());
-                        }
-                    });
-                    $A.enqueueAction(action);
-                }));
-            }
-
-            function enqueueAll(actions) {
-                const promises = actions.map((action) => {
-                    return enqueue.call(this, action.name, action.params, action.options);
-                });
-
-                return Promise.all(promises);
-            }
-
-            return {
-                setContext: setContext,
-                enqueue: enqueue,
-                enqueueAll: enqueueAll
-            };
-        })();
-
-        return this.lax;
-    }
+    return this.lax;
+  },
 });
