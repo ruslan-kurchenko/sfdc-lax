ab998f8b47cd49e5b62afaf66837ac53d7895655
Ruslan Kurchenko <rusl.kurchenko@gmail.com>
2017-12-17 18:31:44 +0200

    Complete JSDocs with objects functions

--- src/aura/lax/laxHelper.js
+++ src/aura/lax/laxHelper.js
@@ -98,13 +98,28 @@
      * The container of the actual context promise.
      * It helps to call chain function (<code>then</code>, <code>catch</code>)
      * in the Aura context. The client can avoid of <code>$A.getCallback</code> calls.
-     * @type {{then: Function, catch: Function}}
+     * @typedef {Object} LaxPromise
      */
     const laxPromise = {
+      /**
+       * The function to assign a success callback on an action
+       * @method
+       * @name LaxPromise#then
+       * @param callback {Function}
+       * @returns {LaxPromise}
+       */
       then: function (callback) {
         const promise = this._contextPromise.then.call(this._contextPromise, $A.getCallback(callback));
         return createAuraContextPromise(promise);
       },
+
+      /**
+       * The function to assign a failure callback on an action
+       * @method
+       * @name LaxPromise#catch
+       * @param callback {Function}
+       * @returns {LaxPromise}
+       */
       catch: function (callback) {
         const promise = this._contextPromise.catch.call(this._contextPromise, $A.getCallback(callback));
         return createAuraContextPromise(promise);
@@ -117,6 +132,7 @@
      * with Aura context functionality. It allows to avoid of <code>$A.getCallback</code>
      * on callback functions.
      * @param promise {Promise}
+     * @returns {LaxPromise}
      */
     function createAuraContextPromise(promise) {
       const props = Object.assign({}, laxPromise);
@@ -134,12 +150,14 @@
      * The object based on builder pattern to call Aura action.
      * It is instantiated to be used by {@link Lax} as a prototype of actual actions.
      * This type of action does not use Promise approach and subsequently can be called as storable.
-     * @typedef {{setThen: Function, setCatch: Function, setParams: Function, setStorable: Function, setBackground: Function, enqueue: Function}} LaxAction
+     * @typedef {Object} LaxAction
      */
     const laxAction = {
 
       /**
        * Assign the success callback on Aura action
+       * @method
+       * @name LaxAction#setThen
        * @param callback {Function}
        * @returns {LaxAction}
        */
@@ -150,6 +168,8 @@
 
       /**
        * Assigns the failure callback on Aura action. This function called when the error occurs.
+       * @method
+       * @name LaxAction#setCatch
        * @param callback {Function}
        * @returns {LaxAction}
        */
@@ -160,6 +180,8 @@
 
       /**
        * Sets parameters for the action.
+       * @method
+       * @name LaxAction#setParams
        * @param params {Object}
        * @returns {LaxAction}
        */
@@ -170,6 +192,8 @@
 
       /**
        * Marks the action as a {@link https://developer.salesforce.com/docs/atlas.en-us.lightning.meta/lightning/controllers_server_storable_actions.htm|Storable}
+       * @method
+       * @name LaxAction#setStorable
        * @returns {LaxAction}
        */
       setStorable: function setStorable() {
@@ -179,6 +203,8 @@
 
       /**
        * Marks the action as a {@link https://developer.salesforce.com/docs/atlas.en-us.lightning.meta/lightning/controllers_server_background_actions.htm|Background}
+       * @method
+       * @name LaxAction#setBackground
        * @returns {LaxAction}
        */
       setBackground: function setBackground() {
@@ -189,6 +215,8 @@
       /**
        * Enqueues the action. The function do not return the object itself and should be
        * called at the end of the builder chain.
+       * @method
+       * @name LaxAction#enqueue
        */
       enqueue: function enqueue() {
         this._action.setCallback(this._component, actionRouter(this._resolveCallback, this._rejectCallback));
@@ -200,23 +228,26 @@
     /**
      * The action main object of the component that is used as a shared prototype across all lax components
      * created in the application. See <code>init</code> function of the laxHelper.js where the lax assigned as prototype.
-     * @typedef {{enqueue: Function, enqueueAll: Function, action: Function}} Lax
+     * @typedef {Object} Lax
      */
     const lax = {
 
       /**
        * The object with list of Aura action options
-       * @typedef {{storable: Boolean, background: Boolean, abortable: Boolean}} ActionOptions
+       * @typedef {{storable: Boolean=, background: Boolean=, abortable: Boolean=}} ActionOptions
        */
 
       /**
        * Enqueues the action by the name and with passed in params and options.
        * The function returns Promise, subsequently client can chain the actions
        * by assigning <code>then</code> callbacks or handle the error by <code>catch</code> callback.
+       * @method
+       * @name Lax#enqueue
        * @param actionName {String} the name of the action (Apex controller method name)
        * @param params {Object} the object that contains parameters for the action
        * @param options {ActionOptions} the object with list of
        * options for the action
+       * @returns {LaxPromise}
        */
       enqueue: function enqueue(actionName, params, options) {
         const self = this;
@@ -243,7 +274,10 @@
        * Enqueues the list of actions parallel.
        * The function return {@link Promise} that subsequently can be used to chain callback.
        * The success callback assigned on the {@link Promise} called after all actions ready and an error have not thrown.
+       * @method
+       * @name Lax#enqueueAll
        * @param actions {{name: String, params: Object, options: ActionOptions}[]}
+       * @returns {LaxPromise}
        */
       enqueueAll: function enqueueAll(actions) {
         const self = this;
@@ -256,6 +290,8 @@
 
       /**
        * Creates the action linked to {@link LaxAction} by the provided name.
+       * @method
+       * @name Lax#action
        * @param actionName {String} the name of the action (Apex controller method)
        * @returns {LaxAction}
        */
