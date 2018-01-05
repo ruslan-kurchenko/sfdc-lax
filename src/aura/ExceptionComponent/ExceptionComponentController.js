e21ccb3e7f14e98e220d2012df1107a734b9ab05
Ruslan Kurchenko <rusl.kurchenko@gmail.com>
2018-01-05 21:14:06 +0200

    Complete error handling
    - error - assign handler for Apex Errors
    - incomplete - assign handler for Incomplete actions
    - catch - assign handler for all errors or for specified

--- src/aura/ExceptionComponent/ExceptionComponentController.js
+++ src/aura/ExceptionComponent/ExceptionComponentController.js
@@ -1,17 +1,32 @@
 ({
   onInit: function (component, event, helper) {
+    // component.lax.enqueue('c.getContacts')
+    //   .then(contacts => {
+    //     component.set('v.contacts', contacts);
+    //
+    //     return component.lax.enqueue('c.getException');
+    //   })
+    //   .catch(function(errors) {
+    //     component.set('v.message', errors[0].message);
+    //   })
+    //   .then(result => {
+    //     console.log('This statement will never be called', result);
+    //   })
+    //   .catch(errors => {
+    //     component.set('v.message', errors[0].message);
+    //   })
+
+    const errors = component.lax.errors;
     component.lax.enqueue('c.getException')
-      .then(result => {
-        console.log('This statement will never be called', result);
-      })
-      .catch(errors => {
-        component.set('v.message', errors[0].message);
-      })
-      .then(() => {
-        return component.lax.enqueue('c.getContacts');
-      })
       .then(contacts => {
         component.set('v.contacts', contacts);
       })
+      .catch(errors.ApexActionError, (e) => {
+        console.log('apex error', e);
+      })
+      .catch(errors.IncompleteActionError, (e) => {
+        console.log('incomplete error', e);
+      });
+
   }
 });
--- src/aura/IncompleteComponent/IncompleteComponentHelper.js
+++ src/aura/IncompleteComponent/IncompleteComponentHelper.js
@@ -1,11 +1,48 @@
 ({
   resolveMessage: function (component) {
+    const errors = component.lax.errors;
     component.lax.enqueue('c.getIncomplete')
       .then(message => {
+        console.log('Got a v.message from the server');
         component.set('v.message', message);
       })
-      .catch(errors => {
-        console.log('it is catch handler, something goes wrong...', errors);
+      .then(() => {
+        console.log('Calling c.getException...');
+        return component.lax.enqueue('c.getException');
+      })
+      .then(() => {
+        Promise.then()
+        console.log('This String should never be displayed!');
+      })
+      .incomplete(e => {
+        console.log('IncompleteActionError', e);
+        component.set('v.message', 'Super, we have handled IncompleteActionError');
+        null.f();
+      })
+      .error(e => {
+        console.log('ApexActionError', e);
+      })
+      .catch(TypeError, (e) => {
+        console.log('TypeError', e);
+      })
+      .then(() => {
+        console.log('Throwing ReferenceError from valid context...');
+        const a = abc;
+      })
+      .then(() => {
+        console.log('This String should never be displayed!');
+      })
+      .catch(ReferenceError, e => {
+        console.log('ReferenceError', e);
+        var float = 1.2345;
+        float.toFixed(123);
+      })
+      .catch(RangeError, e => {
+        console.log('RangeError', e);
+        decodeURI('%foo%bar%');
+      })
+      .catch(URIError, e => {
+        console.log('URIError', e);
       })
   }
 });
